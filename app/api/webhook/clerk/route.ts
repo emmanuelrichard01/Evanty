293e9a71ba738abc156ebcbf9e70b0b0ec9a32da
emmanuelrichard01 <emma.moghalu@gmail.com>
2024-06-03 15:41:59 +0100

    fix import

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { NextResponse } from 'next/server'
+import { clerkClient } from '@clerk/nextjs'
 
 export async function POST(req: Request) {
 
