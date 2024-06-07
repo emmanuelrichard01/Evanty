b77c024778079851bc47f8df54eaf654997712a7
emmanuelrichard01 <emma.moghalu@gmail.com>
2024-06-07 16:19:46 +0100

    Form Submission

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -2,18 +2,19 @@ import { Document, Schema, model, models, Model } from 'mongoose';
 
 // Interface for the Event document
 export interface IEvent extends Document {
-  _id: string
+  _id: string;
   title: string;
   description?: string;
   location?: string;
+  createdAt: Date;
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
   price: string;
   isFree: boolean;
   url?: string;
-  category: { _id: string; name: string };
-  organizer: { _id: string; firstName: string; lastName: string };
+  category: { _id: string, name: string }
+  organizer: { _id: string, firstName: string, lastName: string }
 }
 
 // Event schema definition
@@ -22,34 +23,24 @@ const EventSchema = new Schema<IEvent>(
     title: { type: String, required: true },
     description: { type: String },
     location: { type: String },
+    createdAt: { type: Date, default: Date.now },
     imageUrl: {
       type: String,
       required: true,
       match: [/^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/, 'Please fill a valid URL for the image'],
     },
-    startDateTime: { type: Date, required: true },
-    endDateTime: { type: Date, required: true },
-    price: { type: String, required: true },
+    startDateTime: { type: Date, default: Date.now },
+    endDateTime: { type: Date, default: Date.now },
+    price: { type: String },
     isFree: { type: Boolean, default: false },
     url: { type: String, match: [/^https?:\/\/.+/, 'Please fill a valid URL'] },
-    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-    organizer: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    organizer: { type: Schema.Types.ObjectId, ref: 'User' },
   },
   {
     timestamps: true, // Adds createdAt and updatedAt fields
-    versionKey: false, // Optional: Disable __v field
   }
 );
-
-// Add indexes
-EventSchema.index({ title: 1 });
-EventSchema.index({ startDateTime: 1 });
-
-// Pre-save middleware (optional): Add hooks for validation, logging, etc.
-EventSchema.pre<IEvent>('save', function (next) {
-  // Example: Add custom logic before saving
-  next();
-});
 
 // Static methods
 EventSchema.statics.findByCategory = function (categoryId: string) {
